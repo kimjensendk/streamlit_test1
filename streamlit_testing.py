4608ded5bcdf04119ec0d9afd5221fdd27055934
kimjensendk <68803500+kimjensendk@users.noreply.github.com>
2023-12-02 10:09:21 +0100

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -15,4 +15,9 @@ st.text('txt')
 
 st.dataframe(df2)
 
-st.dataframe(df2.plot())
+df = pd.DataFrame({'length': [1.5, 0.5, 1.2, 0.9, 3],
+                  'width': [0.7, 0.2, 0.15, 0.2, 1.1]},
+                  index=['pig', 'rabbit', 'duck', 'chicken', 'horse'])
+plot = df.plot(title="DataFrame Plot")
+
+
