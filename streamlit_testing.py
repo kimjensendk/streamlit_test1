4b5620575bb5cdc9bce17bcb0a505c60d2c62765
kimjensendk <68803500+kimjensendk@users.noreply.github.com>
2023-12-02 10:03:23 +0100

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('Hello world dette er header'en')
+df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                   columns=['a', 'b', 'c'])
+
+
+
+st.title('Hello world dette er headeren på denn streamlit app')
 
 st.header('header')
 
 st.text('txt')
+
+st.dataframe(df2)
