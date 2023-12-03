771cec650af5931955a7c787ab0133b673d6b0bc
Kim Jensen <kimj.dk@gmail.com>
2023-12-03 13:03:36 +0100

    test cooment

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import yfinance as yf
 
 
-df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-                   columns=['a', 'b', 'c'])
+st.write("""
+# Simple Stock Price app
+Shown are stock closing price and volume of google
+""")
+
+tickerSymbol = 'GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
 
 
+tickerDf = tickerData.history(period='1d', start = '2010-5-31', end='2023-1-31')
 
-st.title('Hello world')
 
-st.header('Nu skal der ')
-
-st.text('Her står der noget standard tekst alm størrelse')
-
-df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
-
-st.dataframe(df.style.highlight_max(axis=0))
-
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
