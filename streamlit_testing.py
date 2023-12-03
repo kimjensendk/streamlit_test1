fa2067219696296a9052fd0ee7f0a88be7aedb8f
Kim Jensen <kimj.dk@gmail.com>
2023-12-03 13:28:33 +0100

    test cooment

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -16,6 +16,8 @@ tickerData = yf.Ticker(tickerSymbol)
 
 tickerDf = tickerData.history(period='1d', start = '2010-5-31', end='2023-1-31')
 
-
+st.write("closing price")
 st.line_chart(tickerDf.Close)
+
+st.write("Volume")
 st.line_chart(tickerDf.Volume)
