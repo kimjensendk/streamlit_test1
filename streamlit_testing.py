76960711c90b91a6d25bb36504e6bd264cd07a2d
Kim Jensen <kimj.dk@gmail.com>
2023-12-03 12:27:24 +0100

    test cooment

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -9,7 +9,7 @@ df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
 
 st.title('Hello wor ld dette er headeren på denn streamlit app')
 
-st.header('Nu skal der stå noget ordentligt')
+st.header('Nu skal der stå noget ordenfdghfhtligt')
 
 st.text('txt')
 
